7dbd68196d2234d68255f657889378b34b8ded11
Yaro <144354667+sukyaro@users.noreply.github.com>
2025-08-19 22:08:52 +0100

    Add files via upload

---
+++ Python For Beginners/2.2PrintInputVariables.py
@@ -0,0 +1,55 @@
+
+# Question 1
+# Prints "Hello World!" to the screen
+print("Hello World!")
+
+# Question 2
+# Prints a sequence of numbers on one line separated by spaces
+print("4", "8", "15", "16", "23", "42")
+
+# Question 3
+# Prints each number on a new line
+print("4")
+print("8")
+print("15")
+print("16")
+print("23")
+print("42")
+
+# Question 4
+# Prints a triangle made of asterisks, each line adds one more
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+print("******")
+print("*******")
+
+# Question 5
+# Takes a name as input and greets the user
+name= input()
+print("Hello,", name)
+
+# Question 6
+# Takes a name as input and prints that the person is the champion
+name = input()
+print(name, "is the champion!")
+
+# Question 7
+# Takes three inputs and prints them in the same order
+a = input()
+b = input()
+c = input()
+print(a)
+print(b)
+print(c)
+
+# Question 8
+# Takes three inputs and prints them in reverse order
+a = input()
+b = input()
+c = input()
+print(c)
+print(b)
+print(a)
